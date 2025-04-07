cc8686b560e0978bf1b2809090a343fd73ce2b38
oscar castilla <castilla510@gmail.com>
2025-04-06 18:42:05 -0600

    control de accesos

--- src/Principal.java
+++ src/Principal.java
@@ -1,28 +1,32 @@
+import com.aluracursos.screen.models.Movies;
+
 public class Principal {
 
     public static void main(String[] args) {
         Movies myMovie = new Movies(); // creamos una nueva instancia- la memoria guarda un espacio para este objeto: myMovie
 
-        myMovie.name = "Harry potter y la piedra filosofal";
-        myMovie.durationInMinutes = 152;
-        myMovie.releaseDate = 2001;
-        myMovie.isIncludedInPlan = true;
+        myMovie.setName("Harry Potter y la piedra filosofal");
+        myMovie.setDurationInMinutes(152);
+        myMovie.setReleaseDate(2001);
+        myMovie.setIncludedInPlan(true);
 
         myMovie.showTechnicalSheet(); // llamada al metodo void que imprime la ficha tecnica de la pelicula
         myMovie.evaluate(10);
         myMovie.evaluate(8);
-
-        System.out.println(myMovie.sumOfEvaluation);
+        System.out.println(myMovie.getAverageOfTheEvaluations());
         System.out.println(myMovie.calculateAverage());
 
 
 
-        Movies otherMovie = new Movies();
 
-        otherMovie.name = "Matrix";
-        otherMovie.durationInMinutes = 125;
-        otherMovie.releaseDate = 1999;
-        otherMovie.isIncludedInPlan = true;
+
+
+//        com.aluracursos.screen.models.Movies otherMovie = new com.aluracursos.screen.models.Movies();
+
+//        otherMovie.name = "Matrix";
+//        otherMovie.durationInMinutes = 125;
+//        otherMovie.releaseDate = 1999;
+//        otherMovie.isIncludedInPlan = true;
 
 //        otherMovie.showTechnicalSheet();
 
